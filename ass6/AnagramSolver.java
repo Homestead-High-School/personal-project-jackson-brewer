2b360dba66ed541158d848c1882c94b586975e6d
Jackson Brewer <breweja@students.mtsd.k12.wi.us>
2024-05-17 08:32:07 -0500

    test

--- ass6/AnagramSolver.java
+++ ass6/AnagramSolver.java
@@ -1,3 +1,58 @@
+import java.util.*;
+
 public class AnagramSolver {
-    
+
+
+    private ArrayList<String> anagram = new ArrayList<String>();
+    private ArrayList<String> dict = new ArrayList<String>();
+    private ArrayList<String> phrase = new ArrayList<String>();
+    private HashMap<Character, Integer> letterInventory = new HashMap<>();
+
+
+
+    //constructs an anagram solver that will use the given list as its dictionary
+    AnagramSolver(List<String> list){
+        for(int i = 0; i < list.size(); i++){ 
+            dict.add(list.get(i)); // adds all the words in the dictionary into the list 
+        }
+        System.out.println(dict);
+    } 
+
+    //use recursive backtracking to find combinations of words that have the same letters as the given string
+    void print(String s, int max){
+        s = s.replaceAll(" ", "");
+        for(int i = 0; i < s.length(); i++){ // need to impliment a case where the imput isnt a letter or number *******
+            phrase.add(s.substring(i,i+1));
+        }
+        System.out.println(phrase);
+        set(s);
+        if(phrase.size() == 0){
+            return;
+        }
+    }
+
+    public void set(String phrase) { // this method sets the letter inventory to keep track of how many of each letter we have in the hash map
+        for(int i = 0; i < phrase.length(); i++){
+            char letter = phrase.charAt(i);
+            if(letterInventory.containsKey(letter)){
+                int x = letterInventory.get(letter)+1; //increments the value in the hashmap
+                letterInventory.put(letter, x);
+            } else {
+                letterInventory.put(letter, 1); // adds the new letter to the map
+            }
+        }
+        System.out.println(letterInventory);
+    }
+
+    public void subtract(char c){
+        int x = letterInventory.get(c);
+        letterInventory.put(c, x--);
+    }
+
+    public String resursion(String s){
+        return " ";
+    }
 }
+
+
+  
